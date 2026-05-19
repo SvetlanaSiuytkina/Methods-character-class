e088ba4cec88e8ab313303aa2a5f6a1f80f0271b
SVETA <svetadan1234@mail.ru>
2026-05-19 22:56:50 +0500

    Initial commit

---
+++ src/Character.js
@@ -0,0 +1,30 @@
+module.exports = Character;
+
+class Character {
+    constructor(level = 1, attack, defence, health = 100) {
+        this.level = level;
+        this.attack = attack;
+        this.defence = defence;
+        this.health = health;
+    }
+
+    levelUp() {
+        if (this.health === 0) {
+            throw new Error('Нельзя повысить уровень умершего');
+        }
+
+        this.level += 1;
+        this.attack *= 1.2;
+        this.defence *= 1.2;
+        this.health = 100;
+    }
+
+    damage(points) {
+        const damage = points * (1 - this.defence / 100);
+        this.health -= damage;
+
+        if (this.health < 0) {
+            this.health = 0;
+        }
+    }
+}
